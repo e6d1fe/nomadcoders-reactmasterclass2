92451f1ae91be53ca6968c17db582da4fc2b5e76
e6d1fe <e6d1fe@gmail.com>
2023-02-09 14:08:13 +0900

    made category buttons

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,8 +1,11 @@
-import { IToDo } from "./atoms";
+import { IToDo, toDoState } from "./atoms";
 import styled from "styled-components";
+import { useSetRecoilState } from "recoil";
 
 const ToDoItems = styled.li`
   margin-left: 10px;
+  margin-bottom: 5px;
+  font-size: 15px;
 `;
 
 const StatusButton = styled.button`
@@ -11,13 +14,20 @@ const StatusButton = styled.button`
   font-weight: 700;
 `;
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (newCategory: IToDo["category"]) => {
+    console.log("I want to go to ", newCategory);
+  };
+
   return (
     <ToDoItems>
       <span>{text}</span>
-      <StatusButton>TO DO</StatusButton>
-      <StatusButton>IN PRORESS</StatusButton>
-      <StatusButton>DONE</StatusButton>
+      {category !== "TO_DO" && <StatusButton onClick={() => onClick("TO_DO")}>TO DO</StatusButton>}
+      {category !== "IN_PROGRESS" && (
+        <StatusButton onClick={() => onClick("IN_PROGRESS")}>IN PRORESS</StatusButton>
+      )}
+      {category !== "DONE" && <StatusButton onClick={() => onClick("DONE")}>DONE</StatusButton>}
     </ToDoItems>
   );
 }
